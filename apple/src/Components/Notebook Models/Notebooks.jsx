3d50ebefa47ecc09ba882f66b4010b026e4faa49
Partha Pratim Das <dasparthapratim02@gmail.com>
2022-03-31 14:46:13 +0530

    d

--- apple/src/Components/Notebook Models/Notebooks.jsx
+++ apple/src/Components/Notebook Models/Notebooks.jsx
@@ -1,4 +1,5 @@
 import "./models.css"
+import {Link} from "react-router-dom"
 
 export const Notebook = () => {
     return (
@@ -8,7 +9,11 @@ export const Notebook = () => {
                 <div>
                     <img src="https://www.apple.com/v/mac/home/bl/images/overview/compare/compare_mba__fchj615oz0yi_large.png" />
                
-                <h4 className="productName">MacBook Air</h4>
+                    <h4 className="productName">MacBook Air</h4>
+                    <p className="price">From $599</p>
+                    <button className="button">Buy</button>
+                    <br/>
+                    <Link to= "#">Learn More</Link>
                 </div>
         </div>
         <div></div>
